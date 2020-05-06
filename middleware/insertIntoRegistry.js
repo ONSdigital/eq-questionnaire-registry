86e7c28e4afea473634a82ccc1d7c253c5416aa4
martyncolmer <martyc1974@hotmail.com>
2020-05-06 14:45:11 +0100

    removed semicolons and other linting

--- middleware/insertIntoRegistry.js
+++ middleware/insertIntoRegistry.js
@@ -1,39 +1,39 @@
-const database = require("../database");
-const { uuid } = require("uuidv4");
-const getQuestionnaireFromPublisher = require("./getQuestionnaireFromPublisher");
+const database = require("../database")
+const { uuid } = require("uuidv4")
+const getQuestionnaireFromPublisher = require("./getQuestionnaireFromPublisher")
 
 const themeLookup = {
   "Northern Ireland": "northernireland",
   ONS: "default",
   Social: "social",
   "UKIS Northern Ireland": "ukis_ni",
-  "UKIS ONS": "ukis",
-};
+  "UKIS ONS": "ukis"
+}
 
 const insertIntoSurveyResister = async (req, res, next) => {
-  let error = false;
-  const data = req.body;
+  let error = false
+  const data = req.body
   try {
     if (!data) {
-      throw new Error("No data in request body");
+      throw new Error("No data in request body")
     }
     for (const doc of data.publishDetails) {
-      const { surveyId: survey_id, formType: form_type, variants } = doc;
+      const { surveyId: survey_id, formType: form_type, variants } = doc
       console.log(
         `Request to publish questionnaire {survey_id: ${survey_id}, form_type: ${form_type}} received`
-      );
-      const qid = uuid();
+      )
+      const qid = uuid()
       for (const {
         author_id,
         language = "en",
         runner_version = "v0",
-        theme,
+        theme
       } of variants) {
-        const questionnaire = await getQuestionnaireFromPublisher(author_id);
-        questionnaire.theme = themeLookup[theme];
-        questionnaire.form_type = form_type;
-        questionnaire.survey_id = survey_id;
-        const { survey_version = "0", title } = questionnaire;
+        const questionnaire = await getQuestionnaireFromPublisher(author_id)
+        questionnaire.theme = themeLookup[theme]
+        questionnaire.form_type = form_type
+        questionnaire.survey_id = survey_id
+        const { survey_version = "0", title } = questionnaire
         const model = {
           author_id,
           survey_id,
@@ -44,28 +44,30 @@ const insertIntoSurveyResister = async (req, res, next) => {
           title,
           language,
           runner_version,
-          qid,
-        };
-        await database.saveQuestionnaire(model);
+          qid
+        }
+        await database.saveQuestionnaire(model)
         console.log(
           `Questionnaire {survey_id: ${survey_id}, form_type: ${form_type}} saved`
-        );
+        )
       }
     }
-  } catch (e) {
-    console.error(e);
-    error = true;
+  }
+  catch (e) {
+    console.error(e)
+    error = true
   }
 
   if (error) {
     res.status(500).json({
-      message: "Sorry, something went wrong inserting into the register",
-    });
-    next();
-  } else {
-    res.status(200).json({ message: "Ok" });
-    next();
+      message: "Sorry, something went wrong inserting into the register"
+    })
+    next()
   }
-};
+  else {
+    res.status(200).json({ message: "Ok" })
+    next()
+  }
+}
 
-module.exports = insertIntoSurveyResister;
+module.exports = insertIntoSurveyResister
--- middleware/insertIntoRegistry.test.js
+++ middleware/insertIntoRegistry.test.js
@@ -1,12 +1,12 @@
-const databases = ["dynamo", "firestore"];
-const mockSchema = require("./mocks/mockSchema");
+const databases = ["dynamo", "firestore"]
+const mockSchema = require("./mocks/mockSchema")
 
 const mockResponse = () => {
-  const res = { questionnaire: mockSchema() };
-  res.status = jest.fn().mockReturnValue(res);
-  res.json = jest.fn().mockReturnValue(res);
-  return res;
-};
+  const res = { questionnaire: mockSchema() }
+  res.status = jest.fn().mockReturnValue(res)
+  res.json = jest.fn().mockReturnValue(res)
+  return res
+}
 
 const mockRequest = () => {
   const req = {
@@ -21,78 +21,78 @@ const mockRequest = () => {
             {
               language: "en",
               theme: "ONS",
-              author_id: "456",
-            },
-          ],
-        },
-      ],
-    },
-  };
-  return req;
-};
+              author_id: "456"
+            }
+          ]
+        }
+      ]
+    }
+  }
+  return req
+}
 
 const mockBadRequest = () => {
   const badReq = {
     body: {
-      surveyId: "1",
-    },
-  };
-  return badReq;
-};
+      surveyId: "1"
+    }
+  }
+  return badReq
+}
 
 const mockDbCall = () => {
   const dbCall = {
     survey_id: "ins_test_001",
-    form_type: "123",
-  };
-  return dbCall;
-};
+    form_type: "123"
+  }
+  return dbCall
+}
 
 describe.each(databases)("testing InsertIntoRegistry", (databaseName) => {
-  let res, req, insertIntoRegistry, database;
-  const next = jest.fn();
+  let res, req, insertIntoRegistry, database
+  const next = jest.fn()
 
   beforeAll(() => {
-    jest.resetModules();
-    process.env.REGISTRY_DATABASE_SOURCE = databaseName;
-    insertIntoRegistry = require("./insertIntoRegistry");
-    database = require("../database");
-    jest.mock("./getQuestionnaireFromPublisher", () => () => mockSchema());
-  });
+    jest.resetModules()
+    process.env.REGISTRY_DATABASE_SOURCE = databaseName
+    insertIntoRegistry = require("./insertIntoRegistry")
+    database = require("../database")
+    jest.mock("./getQuestionnaireFromPublisher", () => () => mockSchema())
+  })
 
   it(`should add a record into the registry using ${databaseName}`, async () => {
-    res = mockResponse();
-    req = mockRequest();
-    await insertIntoRegistry(req, res, next);
-    const checkData = await database.getQuestionnaire(mockDbCall());
-    expect(res.status).toHaveBeenCalledWith(200);
-    expect(res.json).toHaveBeenCalledWith({ message: "Ok" });
+    res = mockResponse()
+    req = mockRequest()
+    await insertIntoRegistry(req, res, next)
+    const checkData = await database.getQuestionnaire(mockDbCall())
+    expect(res.status).toHaveBeenCalledWith(200)
+    expect(res.json).toHaveBeenCalledWith({ message: "Ok" })
     expect(checkData).toEqual(
       expect.objectContaining({ survey_id: "ins_test_001", form_type: "123" })
-    );
-  });
+    )
+  })
 
   it(`should throw an error when sending a bad request using ${databaseName}`, async () => {
-    res = mockResponse();
-    req = mockBadRequest();
-    const spy = jest.spyOn(console, "error").mockImplementation(() => {});
-    await insertIntoRegistry(req, res, next);
-    expect(res.status).toHaveBeenCalledWith(500);
+    res = mockResponse()
+    req = mockBadRequest()
+    const spy = jest.spyOn(console, "error").mockImplementation(() => {})
+    await insertIntoRegistry(req, res, next)
+    expect(res.status).toHaveBeenCalledWith(500)
     expect(res.json).toHaveBeenCalledWith({
-      message: "Sorry, something went wrong inserting into the register",
-    });
-    spy.mockRestore();
-  });
+      message: "Sorry, something went wrong inserting into the register"
+    })
+    spy.mockRestore()
+  })
 
   it(`should throw an error when sending no data using ${databaseName}`, async () => {
-    res = mockResponse();
-    req = {};
-    const spy = jest.spyOn(console, "error").mockImplementation(() => {});
-    await insertIntoRegistry(req, res, next);
-    expect(res.status).toHaveBeenCalledWith(500);
+    res = mockResponse()
+    req = {}
+    const spy = jest.spyOn(console, "error").mockImplementation(() => {})
+    await insertIntoRegistry(req, res, next)
+    expect(res.status).toHaveBeenCalledWith(500)
     expect(res.json).toHaveBeenCalledWith({
-      message: "Sorry, something went wrong inserting into the register",
-    });
-    spy.mockRestore();
-  });
-});
+      message: "Sorry, something went wrong inserting into the register"
+    })
+    spy.mockRestore()
+  })
+})
